7b4334176a999f2ef88ddbdc47a5661edbded84a
Samar Ranjit <samarranjit321@gmail.com>
2025-08-28 18:55:52 -0500

    refactor: remove unused Button import from MemberCard component

--- src/components/MemberCard.tsx
+++ src/components/MemberCard.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import Link from "next/link";
 import { FaLinkedin } from "react-icons/fa";
 import { IoMail } from "react-icons/io5";
-import { Button } from "antd";
 import Image from "next/image";
 
 interface MemberCardProps {
